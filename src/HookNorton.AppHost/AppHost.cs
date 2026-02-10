290e7381ff587414dbe26c6fe0e5fe997280c2f3
Rich Tebb <rich@tebb.io>
2026-02-10 22:13:43 +0000

    Add HTTPS support for frontend in Aspire

--- src/HookNorton.AppHost/AppHost.cs
+++ src/HookNorton.AppHost/AppHost.cs
@@ -4,7 +4,9 @@ var api = builder.AddProject<Projects.HookNorton>("api")
     .WithHttpHealthCheck("/$$/health")
     .WithExternalHttpEndpoints();
 
+#pragma warning disable ASPIRECERTIFICATES001
 builder.AddViteApp("webfrontend", "../frontend")
+    .WithHttpsDeveloperCertificate()
     .WithReference(api)
     .WaitFor(api);
 
